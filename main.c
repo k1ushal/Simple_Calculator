48eccc3316326a670b9f4899abddcb7e308e9125
Kushal Chaudhary <101350450+k1ushal@users.noreply.github.com>
2022-05-21 17:15:33 +0545

    main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Simple Calculator \n You can only do mathematical operations with 2 numbers only,\n such as 6-2 (Don't enter spaces) \n (Addition, Subtraction, Multiply and Divide are supported) \nCreator : https://github.com/k1ushal/\n \n \n");
+	float a, b;
+	char o;
+	scanf("%f", &a);
+	scanf("%c", &o);
+	scanf("%f", &b);
+	if (o == '+')
+	{
+		printf("= %f", a + b);
+	}
+	else if (o == '-')
+	{
+		printf("= %f", a - b);
+	}
+	else if (o == '*')
+	{
+		printf("= %f", a * b);
+	}
+	else if (o == '/')
+	{
+		printf("= %f", a / b);
+	}
+	else
+	{
+		printf("invalid Operation, Please Start Again ( + , - , * , / )");
+	}
+	printf("\n\n\n\n\n\n");
+	printf("Thanks for using this program.\n Upgrades on this program will be updated soon\n- Kushal");
+
+	return 0;
+}
